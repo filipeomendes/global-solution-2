1bafab66432e4e7b314bb59701736d69a67638c7
fernandopaparelli <rm551022@fiap.com.br>
2023-11-19 01:22:48 -0300

    Adicionando cabecalho e rodapé no layout

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -11,7 +11,11 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="pt-br">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <Cabecalho/>
+          {children}
+        <Rodape/>
+        </body>
     </html>
   )
 }
