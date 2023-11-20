484eb974b8386d8e68c9aceb853796794804d1de
vinips1993 <RM550907@fiap.com.br>
2023-11-20 00:04:33 -0300

    Adicionando link para a home no cabecalho

--- src/components/Cabecalho/Cabecalho.jsx
+++ src/components/Cabecalho/Cabecalho.jsx
@@ -2,7 +2,7 @@ export default function Cabecalho() {
     return(
         <header>
             <div className="texto-meningite">
-                <Link href=""><Image src="/logo" alt="Texto escrito Meningite com link para a home" width={100} height={100}></Image></Link>
+                <Link href="/"><Image src="/logo" alt="Texto escrito Meningite com link para a home" width={100} height={100}></Image></Link>
             </div>
             <div className="menu">
                 <nav className="menu-nav">
