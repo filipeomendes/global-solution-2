0f8bfba354bd4df3f7dfe9a5178fc81e0e47819a
vinips1993 <RM550907@fiap.com.br>
2023-11-20 00:04:12 -0300

    Adicionando classes ao cabecalho

--- src/components/Cabecalho/Cabecalho.jsx
+++ src/components/Cabecalho/Cabecalho.jsx
@@ -1,11 +1,11 @@
 export default function Cabecalho() {
     return(
         <header>
-            <div>
-                <Link href=""><Image src="/logo" alt="Logo tipo" width={100} height={100}></Image></Link>
+            <div className="texto-meningite">
+                <Link href=""><Image src="/logo" alt="Texto escrito Meningite com link para a home" width={100} height={100}></Image></Link>
             </div>
-            <div>
-                <nav>
+            <div className="menu">
+                <nav className="menu-nav">
                     <Link>O que é Meningite?</Link>
                     <Link>Como identificar</Link>
                     <Link>Quem somos</Link>
