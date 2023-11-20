519169069c3bce2ccdc836b91b1d26f56b5163f5
vinips1993 <RM550907@fiap.com.br>
2023-11-20 00:08:35 -0300

    Iniciando estrutura do rodapé

---
+++ src/components/Rodape/Rodape.jsx
@@ -0,0 +1,15 @@
+export default function Rodape() {
+    return (
+        <footer>
+            <section>
+                <div>
+                    <nav>
+                        <a href=""></a>
+                        <a href=""></a>
+                        <a href=""></a>
+                    </nav>
+                </div>
+            </section>
+        </footer>
+    )
+}
