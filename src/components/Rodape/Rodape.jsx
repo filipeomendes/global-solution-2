4c5b254f77a6a3bd29e4bc5514fb88d8008f81c8
vinips1993 <RM550907@fiap.com.br>
2023-11-20 00:26:25 -0300

    Adicionando imagens para itens do rodapé

--- src/components/Rodape/Rodape.jsx
+++ src/components/Rodape/Rodape.jsx
@@ -4,10 +4,13 @@ export default function Rodape() {
             <section>
                 <div>
                     <nav>
-                        <a href=""></a>
-                        <a href=""></a>
-                        <a href=""></a>
+                        <a href=""><Image src="/github.png" alt="Logo preto do Github" width={30} height={30}/></a>
+                        <a href=""><Image src="/instagram.png" alt="Logo preto do Instagram" width={30} height={30}/></a>
+                        <a href=""><Image src="/facebook.png" alt="Logo preto do Facebook" width={30} height={30} /></a>
                     </nav>
+                </div>
+                <div>
+                    <p>Todos os direitos reservados ©2023 - Complex</p>
                 </div>
             </section>
         </footer>
