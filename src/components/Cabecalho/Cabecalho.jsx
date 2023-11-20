1fe201f871e6bf9986f2057a94700f9cba94868a
vinips1993 <RM550907@fiap.com.br>
2023-11-19 23:58:30 -0300

    Iniciando estrutura do cabecalho

---
+++ src/components/Cabecalho/Cabecalho.jsx
@@ -0,0 +1,14 @@
+export default function Cabecalho() {
+    return(
+        <header>
+            <div>
+                <Link href=""><Image src="/logo" alt="Logo tipo" width={100} height={100}></Image></Link>
+            </div>
+            <div>
+                <nav>
+                    <Link></Link>
+                </nav>
+            </div>
+        </header>
+    )
+}
