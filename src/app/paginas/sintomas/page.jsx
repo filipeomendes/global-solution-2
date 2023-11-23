a2407e909ea76fc68a721bddfdb425ff1184ec6d
pauloh-andrade <pauloh8755@gmail.com>
2023-11-23 01:28:31 -0300

    Adicionando classes e imagens

--- src/app/paginas/sintomas/page.jsx
+++ src/app/paginas/sintomas/page.jsx
@@ -1,3 +1,5 @@
+import Image from "next/image"
+
 export const metadata = {
     title: 'Como Identificar'
 }
@@ -6,10 +8,10 @@ export default function Identificar() {
     return (
         <main>
             <section>
-                <div>
+                <div className="texto">
                     <h1>Sintomas e Identificação da Meningite:</h1>
                     <p>A meningite, caracterizada pela inflamação das meninges, apresenta uma variedade de sintomas que podem se manifestar rapidamente. O reconhecimento precoce desses sinais é crucial para buscar assistência médica imediata. Aqui estão os principais sintomas e indicadores que podem ajudar na identificação da meningite:</p>
-                    <div>
+                    <div className="texto-secundario">
                         <p><b>Febre:</b>A febre é frequentemente um dos primeiros sinais de meningite. Uma elevação rápida e significativa da temperatura corporal pode ocorrer.</p>
                         <p><b>Dor de Cabeça Intensa:</b>A dor de cabeça associada à meningite é caracterizada por intensidade e persistência. A sensação pode ser descrita como latejante e muitas vezes não melhora com analgésicos comuns.</p>
                         <p><b>Rigidez no Pescoço:</b>A rigidez no pescoço é um sintoma clássico de meningite. Dificuldade em inclinar o pescoço para frente devido à resistência muscular é um sinal importante</p>
@@ -18,9 +20,15 @@ export default function Identificar() {
                         <p><b>Manchas na Pele (Púrpura):</b>Em casos de meningite bacteriana, podem ocorrer manchas na pele, também conhecidas como púrpura. Essas manchas não desaparecem quando pressionadas e são um sinal de emergência.</p>
                         <p><b>Irritabilidade em Bebês e Crianças Pequenas:</b>Em bebês e crianças pequenas, a meningite pode se manifestar como irritabilidade, choro persistente, recusa em se alimentar e dificuldade em ser acalmado.</p>
                     </div>
+                    <div className="sintomas-img">
+                        <Image src="/sintomas.png" alt="Ilustração com os sintomas da meningite" width={600} height={380}/>
+                    </div>
                     <h1>Identificação Precoce:</h1>
                     <p>Identificar precocemente os sintomas da meningite é crucial para iniciar o tratamento imediatamente. Se houver suspeita de meningite, especialmente ao observar vários dos sintomas mencionados, é essencial procurar atendimento médico de urgência. A meningite bacteriana, em particular, pode progredir rapidamente, tornando a prontidão para a busca de assistência fundamental para um prognóstico positivo.</p>
                     <p>A observação cuidadosa dos sintomas e a resposta rápida diante de qualquer sinal de alerta são a chave para lidar eficazmente com a meningite. O diagnóstico definitivo geralmente requer exames médicos, como análise de líquido cerebroespinhal por meio de uma punção lombar, e o tratamento precoce pode fazer a diferença na recuperação do paciente.</p>
+                </div>
+                <div className="consulta-img">
+                    <Image src="/doctor.jpg" alt="medico em consulta com pacientes" width={600} height={380}/>
                 </div>
             </section>
         </main>
