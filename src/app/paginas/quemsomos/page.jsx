f9f217a5402acebbf6fa50955c45ab9623769319
pauloh-andrade <pauloh8755@gmail.com>
2023-11-23 00:46:24 -0300

    complementando texto "quem somos"

--- src/app/paginas/quemsomos/page.jsx
+++ src/app/paginas/quemsomos/page.jsx
@@ -10,7 +10,7 @@ export default function Sobre() {
                 <p>Olá, somos Fernando, Filipe, Miron, Paulo e Vinicius!</p>
                 <p>Somos alunos da Faculdade Paulista de Administração e Informática - FIAP no curso de Análise e Desenvolvimento de Sistemas e fomos desafiados a ajudar de alguma forma problemas de saúde, com base nas metas da <b>ODS 3</b>.</p>
                 <p>Com base nisso, nosso grupo criou o sistema Vital Support, com foco em ajudar a combater a meningite, visto que a doença aprensentou um crescimento de 123% entre 2021 e 2022, e 2023 também continua em tendência de alta.</p>
-                <p>Nós criamos um sistema informativo onde pessoas podem se informar sobre a doença</p>
+                <p>Nós criamos um sistema informativo onde pessoas podem se informar sobre a doença e nós indicamos clínicas e médicos próximos. Além disso, possuímos um sistema de cadastro onde as clínicas podem se cadastrar e serem indicadas, e também parceiros podem fazer doações para que esse dinheiro seja convertido em ajuda do tratamento de vítimas da meningite.</p>
             </section>
         </main>
     )
