234a1a9028ba0c38bd58d639b2f77ba63fbb8a8c
filipeomendes <filipedeoliveiramendes@gmail.com>
2023-11-24 22:16:56 -0300

    Finalizando

--- src/app/paginas/ajuda/maps/page.jsx
+++ src/app/paginas/ajuda/maps/page.jsx
@@ -4,10 +4,9 @@ import React, { useEffect } from 'react';
 const Home = () => {
   useEffect(() => {
     initMap();
-  }, []); // Run only once on component mount
+  }, []);
 
   const initMap = async () => {
-    // Initialize and add the map
     let map;
 
     const position = { lat: -23.564062217559737, lng: -46.6523911546908 };
@@ -30,8 +29,6 @@ const Home = () => {
   return (
     <div>
       <div id="map" style={{ height: '100vh', width: '100%' }}></div>
-
-      {/* Add your script directly here */}
       <script
         dangerouslySetInnerHTML={{
           __html: `(g=>{var h,a,k,p="The Google Maps JavaScript API",c="google",l="importLibrary",q="__ib__",m=document,b=window;b=b[c]||(b[c]={});var d=b.maps||(b.maps={}),r=new Set,e=new URLSearchParams,u=()=>h||(h=new Promise(async(f,n)=>{await (a=m.createElement("script"));e.set("libraries",[...r]+"");for(k in g)e.set(k.replace(/[A-Z]/g,t=>"_"+t[0].toLowerCase()),g[k]);e.set("callback",c+".maps."+q);a.src="https://maps."+c+"apis.com/maps/api/js?"+e;d[q]=f;a.onerror=()=>h=n(Error(p+" could not load."));a.nonce=m.querySelector("script[nonce]")?.nonce||"";m.head.append(a)}));d[l]?console.warn(p+" only loads once. Ignoring:",g):d[l]=(f,...n)=>r.add(f)&&u().then(()=>d[l](f,...n))})({key: "AIzaSyB0ie1d9o1lw7_o2UnbbaRpdzMYNwLZvDM", v: "beta"});`,
